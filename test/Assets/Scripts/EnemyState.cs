ecd68eb44e844e349540fb5ee80bc8e479692f37
kimha578 <kimha578@naver.com>
2018-05-23 16:26:30 +0900

    적 추가

---
+++ test/Assets/Scripts/EnemyState.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyState 
+{
+    public virtual void Enter(GameObject obj)
+    {
+
+    }
+
+    public virtual void Execute(GameObject obj)
+    {
+
+    }
+
+    public virtual void Exit(GameObject obj)
+    {
+
+    }
+
+	
+}
+public class EnemyIdle: EnemyState
+{
+    GameObject charOBJ;
+    public override void Enter(GameObject obj)
+    {
+        charOBJ = obj;
+
+
+    }
+
+    public override void Execute(GameObject obj)
+    {
+        
+    }
+
+    public override void Exit(GameObject obj)
+    {
+
+    }
+
+
+}
+public class EnemyMove: EnemyState
+{
+    GameObject charOBJ;
+    public override void Enter(GameObject obj)
+    {
+        charOBJ = obj;
+
+
+    }
+
+    public override void Execute(GameObject obj)
+    {
+
+    }
+
+    public override void Exit(GameObject obj)
+    {
+
+    }
+
+
+}
+public class EnemyAttack: EnemyState
+{
+    GameObject charOBJ;
+    public override void Enter(GameObject obj)
+    {
+        charOBJ = obj;
+
+
+    }
+
+    public override void Execute(GameObject obj)
+    {
+
+    }
+
+    public override void Exit(GameObject obj)
+    {
+
+    }
+
+
+}
+public class EnemyDeath: EnemyState
+{
+    GameObject charOBJ;
+    public override void Enter(GameObject obj)
+    {
+        charOBJ = obj;
+
+
+    }
+
+    public override void Execute(GameObject obj)
+    {
+
+    }
+
+    public override void Exit(GameObject obj)
+    {
+
+    }
+
+
+}
+public class EnemySkill: EnemyState
+{
+    GameObject charOBJ;
+    public override void Enter(GameObject obj)
+    {
+        charOBJ = obj;
+
+
+    }
+
+    public override void Execute(GameObject obj)
+    {
+
+    }
+
+    public override void Exit(GameObject obj)
+    {
+
+    }
+
+
+}
+
